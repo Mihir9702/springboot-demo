5c60ca162b57c3ab947ec848aaf64b958e532cf7
Mihir9702 <mkpatel9702@gmail.com>
2024-02-15 13:26:24 -0500

    msg

---
+++ src/main/java/com/techelevator/randomproject/RandomprojectApplication.java
@@ -0,0 +1,13 @@
+package com.techelevator.randomproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techelevator/randomproject/RandomprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techelevator.randomproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
